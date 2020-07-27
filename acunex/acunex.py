93884e2e69f706868ebb649e3b3beb85d3bae2e2
Edgecase963 <edgecase963@gmail.com>
2020-07-27 14:06:45 -0400

    Dev update
    Large updates to the development code - still a work in progress..

--- acunex/acunex.py
+++ acunex/acunex.py
@@ -1,14 +1,46 @@
 #!/usr/bin/env python
 import torch
 from torch import tensor
+import matplotlib.pyplot as plt
 
 
 
 class Trainer():
-    def __init__(self, model, learning_rate=0.001, reduction='mean'):
+    def __init__(self, model, optimizer="adam", learning_rate="default", reduction='mean'):
+        self.optimizers = {
+            "adam": torch.optim.Adam,
+            "adadelta": torch.optim.Adadelta,
+            "adagrad": torch.optim.Adagrad,
+            "adamax": torch.optim.Adamax,
+            "adamw": torch.optim.AdamW,
+            "asgd": torch.optim.ASGD,
+            "rmsprop": torch.optim.RMSprop,
+            "rprop": torch.optim.Rprop,
+            "sgd": torch.optim.SGD,
+        }
+
+        if optimizer == "sgd" and learning_rate == "default":
+            learning_rate = 0.1
+
+        self.learning_rate = learning_rate
         self.loss_fn = torch.nn.MSELoss(reduction=reduction)
-        self.optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         self.model = model
+        self.iterations = 0
+        self.history = {}   # Structure: {<epoch>: <loss>}
+        self.optimizer_name = optimizer
+
+        if self.learning_rate == "default":
+            self.optimizer = self.optimizers[optimizer.lower()](model.parameters())
+            self.learning_rate = self.optimizer.defaults["lr"]
+        else:
+            self.optimizer = self.optimizers[optimizer.lower()](model.parameters(), lr=self.learning_rate)
+
+    def set_optimizer(self, optimizer):
+        self.optimizer = self.optimizers[optimizer.lower()](self.model.parameters(), lr=self.learning_rate)
+
+    def set_learning_rate(self, learning_rate):
+        self.learning_rate = learning_rate
+        self.optimizer.defaults["lr"] = learning_rate
 
     def train_epoch(self, inputData, target):
         result = self.model(inputData)
@@ -18,6 +50,9 @@ class Trainer():
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
+
+        self.iterations += 1
+        self.history[self.iterations] = loss.item()
 
         return loss.item()
 
@@ -32,6 +67,7 @@ class Trainer():
                         if target_loss:
                             if loss <= target_loss:
                                 return loss
+                    return loss
             elif target_loss:
                 loss = target_loss+1.0
                 while loss > target_loss:
@@ -60,11 +96,21 @@ if __name__ == "__main__":
 
     trainer = Trainer(network)
 
+    lossList = []
+    epochList = []
+
     for i in range(10000):
-        loss = trainer.train_epoch(inputData, targetData)
-        if not i % 10:
-            print(i, loss)
+        loss = trainer.trainUntil(inputData, targetData, iterations=20)
+        print(trainer.iterations, loss)
+        lossList.append(loss)
+        epochList.append(trainer.iterations)
         if loss < 0.001:
             break
+    for epoch in trainer.history:
+        plt.plot(epochList, lossList)
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Optimizer: {}".format(trainer.optimizer_name))
+    plt.show()
 
     print(network(inputData))
